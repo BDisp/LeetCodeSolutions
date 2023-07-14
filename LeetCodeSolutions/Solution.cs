cf4857251e72df8ee096c917ad8f6f5901898f8e
BDisp <bd.bdisp@gmail.com>
2023-07-14 17:54:37 +0100

    Add reverse search.

--- LeetCodeSolutions/Solution.cs
+++ LeetCodeSolutions/Solution.cs
@@ -1,30 +1,53 @@
-﻿namespace LeetCodeSolutions;
+﻿using System.Linq;
+
+namespace LeetCodeSolutions;
 
 public class Solution
 {
     public bool WordBreak(string s, IList<string> wordDict)
     {
-        var r = s;
-        for (int i = 0; i < wordDict.Count; i++)
+        var isSameChars = s.Distinct().Count() == 1 && wordDict.All(x => x[0] == s[0] && x.Distinct().Count() == 1);
+        var maxItemLength = wordDict.Aggregate((max, cur) => max.Length > cur.Length ? max : cur).Length;
+        var idx = isSameChars ? s.Length : 0;
+        var length = isSameChars ? maxItemLength : 1;
+        while (true)
         {
-            string? w = wordDict[i];
-            while (true)
+            if (isSameChars)
             {
-                var pos = r.IndexOf(w);
-                if (pos >= 0)
+                var sb = s.Substring(idx - length, length);
+                var found = wordDict.Contains(sb);
+                if (found)
                 {
-                    r = r.Remove(pos, w.Length);
+                    idx -= length;
+                    length = 1;
                 }
                 else
                 {
-                    break;
+                    length++;
+                }
+                if (idx == 0 || idx - length < 0)
+                {
+                    return found;
                 }
             }
-            if (r.Length == 0)
+            else
             {
-                return true;
+                var sb = s.Substring(idx, length);
+                var found = wordDict.Contains(sb);
+                if (found)
+                {
+                    idx += length;
+                    length = 1;
+                }
+                else
+                {
+                    length++;
+                }
+                if (idx == s.Length || idx + length > s.Length)
+                {
+                    return found;
+                }
             }
         }
-        return r.Length == 0;
     }
 }
--- LeetCodeSolutionsTests/LeetCodeSolutionsTests.cs
+++ LeetCodeSolutionsTests/LeetCodeSolutionsTests.cs
@@ -9,7 +9,8 @@ public class LeetCodeSolutionsTests : Solution
     [InlineData("applepenapple", new string[] { "apple", "pen" }, true)]
     [InlineData("catsandog", new string[] { "cats", "dog", "sand", "and", "cat" }, false)]
     [InlineData("bb", new string[] { "a", "b", "bbb", "bbbb" }, true)]
-    //[InlineData("cars", new string[] { "car", "ca", "rs" }, true)]
+    [InlineData("cars", new string[] { "car", "ca", "rs" }, true)]
+    [InlineData("aaaaaaa", new string[] { "aaaa", "aaa" }, true)]
     public void WordBreakTests(string s, IList<string> wordDict, bool expected)
     {
         Assert.Equal(expected, WordBreak(s, wordDict));
